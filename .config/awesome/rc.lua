b0433bb51b53ab004955c18f111c70c1362d7a1f
Kay Kaprolat <kakaoh6@gmail.com>
2020-01-19 10:15:25 +0100

    changed terminal to kitty for ligatures

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -58,7 +58,7 @@ end
 beautiful.init("~/.config/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "alacritty"
+terminal = "kitty"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
