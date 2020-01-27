f46289e57d5476e94cbcd67f028346ab7e0ae4a4
Kay Kaprolat <Frank_Kay.Kaprolat@mailbox.tu-dresden.de>
2020-01-27 20:56:31 +0100

    added padding to wifi widget

--- .config/awesome/widgets/wifi.lua
+++ .config/awesome/widgets/wifi.lua
@@ -3,21 +3,22 @@ local wibox = require('wibox')
 local beautiful = require('beautiful')
 
 -- arrays start with 1 in lua
-local normal = { '冷', '爛', '嵐', '襤', '蠟' }
-local alert =  { '浪', '蘭', '濫', '拉', '廊' }
-local lock =   { '狼', '鸞', '藍', '臘', '朗' }
-local disconnected = '郎'
+-- add 1 space padding around the icons
+local normal = { ' 冷 ', ' 爛 ', ' 嵐 ', ' 襤 ', ' 蠟 ' }
+local alert =  { ' 浪 ', ' 蘭 ', ' 濫 ', ' 拉 ', ' 廊 ' }
+local lock =   { ' 狼 ', ' 鸞 ', ' 藍 ', ' 臘 ', ' 朗 ' }
+local disconnected = ' 郎 '
 
 local widget = wibox.widget {
         {
-                id = 'wifi_icon',
-                markup = disconnected,
-                font = 'Material Design Icons',
-                widget = wibox.widget.textbox,
-                align = 'center',
-                valign = 'center'
-        },
-        layout = wibox.layout.fixed.horizontal,
-        color = beautiful.red
+                        widget = wibox.widget.textbox,
+                        markup = alert[1],
+                        align = 'center',
+                        valign = 'center',
+                        font = 'Material Design Icons 15',
+                        id = 'wifi_icon'
+                },
+                fg = beautiful.red, -- does not work
+                widget = wibox.container.background
 }
 return widget
