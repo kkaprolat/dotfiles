ba5b561f4ad4687e3bbe4bc5933efc8ff535686d
Kay Kaprolat <Frank_Kay.Kaprolat@mailbox.tu-dresden.de>
2020-01-27 19:38:37 +0100

    expanded wifi widget

--- .config/awesome/widgets/wifi.lua
+++ .config/awesome/widgets/wifi.lua
@@ -1,15 +1,23 @@
 local awful = require('awful')
 local wibox = require('wibox')
+local beautiful = require('beautiful')
+
+-- arrays start with 1 in lua
+local normal = { '冷', '爛', '嵐', '襤', '蠟' }
+local alert =  { '浪', '蘭', '濫', '拉', '廊' }
+local lock =   { '狼', '鸞', '藍', '臘', '朗' }
+local disconnected = '郎'
 
 local widget = wibox.widget {
         {
                 id = 'wifi_icon',
-                markup = '爛',
+                markup = disconnected,
                 font = 'Material Design Icons',
                 widget = wibox.widget.textbox,
                 align = 'center',
                 valign = 'center'
         },
-        layout = wibox.layout.fixed.horizontal
+        layout = wibox.layout.fixed.horizontal,
+        color = beautiful.red
 }
 return widget
