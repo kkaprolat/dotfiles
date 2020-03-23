422ed376785c26496c0851f5518396e1f3c7ae3f
Kay Kaprolat <Frank_Kay.Kaprolat@mailbox.tu-dresden.de>
2020-03-23 14:59:36 +0100

    fixed volume scripts

--- Scripts/volup.sh
+++ Scripts/volup.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-
-pactl set-sink-volume 0 +3%
-pactl set-sink-volume 1 +3%
-notify-send --urgency=low --hint=string:x-dunst-stack-tag:volume "Volume : $(pactl list sink-inputs | grep -A15 -P "(\#|№)$SINK" | grep -P "\d+\s*\/\s*\d+\%" | head -1 | awk "{print \$5}")"
+# see: https://unix.stackexchange.com/questions/132230/read-out-pulseaudio-volume-from-commandline-i-want-pactl-get-sink-volume
+SINK=$( pactl list short sinks | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' | head -n 1 )
+pactl set-sink-volume $SINK +3%
+NOW=$( pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,' )
+notify-send --urgency=low --hint=string:x-dunst-stack-tag:volume "Volume : $NOW%"
