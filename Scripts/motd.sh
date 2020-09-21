b0b4de6c0fdc847f9b5137f86b9a6972d702505f
Kay Kaprolat <Frank_Kay.Kaprolat@mailbox.tu-dresden.de>
2020-09-21 12:56:57 +0200

    Update notification and pacnew reminder

--- Scripts/motd.sh
+++ Scripts/motd.sh
@@ -6,4 +6,10 @@ updatecount=$(checkupdates | wc -l)
 if [ $updatecount != 0 ]
 then
 echo "    There are ${updatecount} updates available." >> $motdfile
+notify-send Updates "${updatecount} updates are available."
+fi
+pacnewcount=$(find /etc -regextype posix-extended -regex ".+\.pacnew" 2> /dev/null | wc -l)
+if [ $pacnewcount != 0 ]
+then
+echo "    There are ${pacnewcount} .pacnew-files to be merged." >> $motdfile
 fi
