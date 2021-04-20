0d8876a1c6d48b2a13f153463ddf2b8d572295ae
Kay Kaprolat <Frank_Kay.Kaprolat@mailbox.tu-dresden.de>
2021-04-20 19:00:16 +0200

    also count AUR packages for updates

--- Scripts/motd.sh
+++ Scripts/motd.sh
@@ -2,7 +2,7 @@
 motdfile=$HOME/.motd
 : > $motdfile
 neofetch >> $motdfile
-updatecount=$(checkupdates | wc -l)
+updatecount=$((checkupdates; yay -Qu;) | wc -l)
 if [ $updatecount != 0 ]
 then
 echo "    There are ${updatecount} updates available." >> $motdfile
