cab4340af63f52335200dbf883a5fb317ffd6c7a
Kay Kaprolat <Frank_Kay.Kaprolat@mailbox.tu-dresden.de>
2021-06-17 18:14:02 +0200

    added backlight control for normal monitor

--- Scripts/backlightdown.sh
+++ Scripts/backlightdown.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-xbacklight -dec 5
-notify-send "Brightness" $(xbacklight -get)
+if [ $(hostname) == Magnesium ]
+then
+        xbacklight -dec 5
+        notify-send "Brightness" $(xbacklight -get)
+        fi
+if [ $(hostname) == Technetium ]
+then
+        before=$(ddcutil getvcp 10 | sed 's/ //g' | sed 's/VCPcode0x10(Brightness):currentvalue=//' | sed 's/,maxvalue=100//')
+        ddcutil setvcp 10 $(($before-5))
+        after=$(($before-5))
+        notify-send "Brightness" $after
+fi
--- Scripts/backlightup.sh
+++ Scripts/backlightup.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-xbacklight -inc 5
-notify-send "Brightness" $(xbacklight -get)
+if [ $(hostname) == Magnesium ]
+then
+        xbacklight -inc 5
+        notify-send "Brightness" $(xbacklight -get)
+fi
+if [ $(hostname) == Technetium ]
+then
+        before=$(ddcutil getvcp 10 | sed 's/ //g' | sed 's/VCPcode0x10(Brightness):currentvalue=//' | sed 's/,maxvalue=100//')
+        ddcutil setvcp 10 $(($before + 5))
+        after=$(($before + 5))
+        notify-send "Brightness" $after
+fi
+
+
