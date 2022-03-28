b278de8ecf4a34d9660098f437503536c411cfb2
Kay Kaprolat <Frank_Kay.Kaprolat@mailbox.tu-dresden.de>
2022-03-28 19:37:44 +0200

    added SecureBoot check to MOTD

--- Scripts/motd.sh
+++ Scripts/motd.sh
@@ -6,10 +6,15 @@ updatecount=$((checkupdates; yay -Qu;) | wc -l)
 if [ $updatecount != 0 ]
 then
 echo "    There are ${updatecount} updates available." >> $motdfile
-notify-send.py Updates "${updatecount} updates are available." --hint string:image-path:software-update-available
+dunstify Updates "${updatecount} updates are available." --icon="software-update-available-symbolic.symbolic"
 fi
 pacnewcount=$(find /etc -regextype posix-extended -regex ".+\.pacnew" 2> /dev/null | wc -l)
 if [ $pacnewcount != 0 ]
 then
 echo "    There are ${pacnewcount} .pacnew-files to be merged." >> $motdfile
+fi
+sbstate=$((mokutil --sb-state;))
+if [ $sbstate != "SecureBoot enabled" ]
+then
+echo "    SecureBoot is disabled!"
 fi
