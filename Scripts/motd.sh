91b621ec5910db10b7368c0ba1379415439bd834
Kay Kaprolat <Frank_Kay.Kaprolat@mailbox.tu-dresden.de>
2022-03-28 19:39:56 +0200

    fixed issues in MOTD script

--- Scripts/motd.sh
+++ Scripts/motd.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 motdfile=$HOME/.motd
-: > $motdfile
-neofetch >> $motdfile
-updatecount=$((checkupdates; yay -Qu;) | wc -l)
-if [ $updatecount != 0 ]
+: > "$motdfile"
+neofetch >> "$motdfile"
+updatecount=$( (checkupdates; yay -Qu;) | wc -l)
+if [ "$updatecount" != 0 ]
 then
-echo "    There are ${updatecount} updates available." >> $motdfile
+echo "    There are ${updatecount} updates available." >> "$motdfile"
 dunstify Updates "${updatecount} updates are available." --icon="software-update-available-symbolic.symbolic"
 fi
 pacnewcount=$(find /etc -regextype posix-extended -regex ".+\.pacnew" 2> /dev/null | wc -l)
-if [ $pacnewcount != 0 ]
+if [ "$pacnewcount" != 0 ]
 then
-echo "    There are ${pacnewcount} .pacnew-files to be merged." >> $motdfile
+echo "    There are ${pacnewcount} .pacnew-files to be merged." >> "$motdfile"
 fi
-sbstate=$((mokutil --sb-state;))
-if [ $sbstate != "SecureBoot enabled" ]
+sbstate=$( (mokutil --sb-state;) )
+if [ "$sbstate" != "SecureBoot enabled" ]
 then
 echo "    SecureBoot is disabled!"
 fi
