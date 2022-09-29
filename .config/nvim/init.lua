93a5b373ad56c8fc4aec3f1af0388e644d1d6fe0
Kay Kaprolat <Frank_Kay.Kaprolat@mailbox.tu-dresden.de>
2022-09-29 20:28:19 +0200

    move nvim leap config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,13 +19,3 @@ require'lush'(require'lush_theme.my_theme')
 
 -- heirline
 require'my_heirline'
-
--- leap
-local function leap_all_windows()
-        local focusable_windows_on_tabpage = vim.tbl_filter(
-                function (win) return vim.api.nvim_win_get_config(win).focusable end,
-                vim.api.nvim_tabpage_list_wins(0)
-        )
-        require'leap'.leap { target_windows = focusable_windows_on_tabpage }
-end
-vim.keymap.set('n', 's', leap_all_windows, { silent = true })
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -68,7 +68,18 @@ return require('packer').startup(function(use)
         use { 'romgrk/barbar.nvim',
                 requires = {'kyazdani42/nvim-web-devicons'}
         }
-        use 'ggandor/leap.nvim'
+        use { 'ggandor/leap.nvim',
+                config = function()
+                        local function leap_all_windows()
+                                local focusable_windows_on_tabpage = vim.tbl_filter(
+                                        function (win) return vim.api.nvim_win_get_config(win).focusable end,
+                                        vim.api.nvim_tabpage_list_wins(0)
+                                )
+                                require'leap'.leap { target_windows = focusable_windows_on_tabpage }
+                        end
+                        vim.keymap.set('n', 's', leap_all_windows, { silent = true })
+                end
+        }
 
         use { 'folke/which-key.nvim',
                 config = function()
