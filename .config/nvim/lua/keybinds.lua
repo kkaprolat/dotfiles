ac963e2f6e8f079a758f11f0c83395b3a6e1ba89
Kay Kaprolat <Frank_Kay.Kaprolat@mailbox.tu-dresden.de>
2023-01-31 20:35:03 +0100

    nvim: remove outdated keybindings

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -25,22 +25,3 @@ local check_back_space = function()
     end
 end
 
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
-_G.tab_complete = function()
-    if vim.fn.pumvisible() == 1 then
-        return t "<C-n>"
-    elseif check_back_space() then
-        return t "<Tab>"
-    else
-        return vim.fn['compe#complete']()
-    end
-end
-_G.s_tab_complete = function()
-    if vim.fn.pumvisible() == 1 then
-        return t "<C-p>"
-    else
-        return t "<S-Tab>"
-    end
-end
