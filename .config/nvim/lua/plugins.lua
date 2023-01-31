30060357347178660e15f7c871200d72ace98243
Kay Kaprolat <Frank_Kay.Kaprolat@mailbox.tu-dresden.de>
2023-01-31 20:37:57 +0100

    nvim: format plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -154,238 +154,239 @@ return {
                     end
                 }
             }
-        end,
-        dependencies = {
-            'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-path',
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-nvim-lua',
-            'onsails/lspkind.nvim',
-            'kyazdani42/nvim-web-devicons',
-            'saadparwaiz1/cmp_luasnip',
-        }
-    },
+    end,
+    dependencies = {
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-nvim-lua',
+        'onsails/lspkind.nvim',
+        'kyazdani42/nvim-web-devicons',
+        'saadparwaiz1/cmp_luasnip',
+    }
+},
 
-    -- Snippets
-    'L3MON4D3/LuaSnip',
-    'rafamadriz/friendly-snippets',
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
-        event = "BufReadPost",
-        dependencies = {
-            'nvim-treesitter/playground'
+-- Snippets
+'L3MON4D3/LuaSnip',
+'rafamadriz/friendly-snippets',
+{
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    event = "BufReadPost",
+    dependencies = {
+        'nvim-treesitter/playground'
+    },
+    opts = {
+        ensure_installed = {
+            'bash',
+            'c',
+            'cpp',
+            'javascript',
+            'json',
+            'lua',
+            'markdown',
+            'markdown_inline',
+            'python',
+            'regex',
+            'vim',
         },
-        opts = {
-                ensure_installed = {
-                    'bash',
-                    'c',
-                    'cpp',
-                    'javascript',
-                    'json',
-                    'lua',
-                    'markdown',
-                    'markdown_inline',
-                    'python',
-                    'regex',
-                    'vim',
-                },
-                highlight = {
-                    enable = true,
-                },
-                indent = {
-                    enable = true,
-                },
-            }
-    },
-    {
-        'lervag/vimtex', -- LaTeX
-        ft = {"tex", "bib"},
-        config = function()
-            vim.g.vimtex_view_method = 'zathura'
-            vim.g.vimtex_compiler_progname = 'nvr'
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
+        indent = {
+            enable = true,
+        },
+    }
+},
+{
+    'lervag/vimtex', -- LaTeX
+    ft = {"tex", "bib"},
+    config = function()
+        vim.g.vimtex_view_method = 'zathura'
+        vim.g.vimtex_compiler_progname = 'nvr'
+    end
+},
+{
+    'rktjmp/lush.nvim', -- color schemes
+    lazy = false, -- load it during startup
+    priority = 1000, -- load before other start plugins
+    config = function()
+        require'lush'(require'lush_theme.my_theme')
+    end
+},
+{
+    'romgrk/barbar.nvim',
+    dependencies = {
+        'kyazdani42/nvim-web-devicons'
+    }
+},
+{
+    'ggandor/leap.nvim',
+    event = "VeryLazy",
+    config = function()
+        local function leap_all_windows()
+            local focusable_windows_on_tabpage = vim.tbl_filter(
+            function (win) return vim.api.nvim_win_get_config(win).focusable end,
+            vim.api.nvim_tabpage_list_wins(0)
+            )
+            require'leap'.leap { target_windows = focusable_windows_on_tabpage }
         end
-    },
-    {
-        'rktjmp/lush.nvim', -- color schemes
-        lazy = false, -- load it during startup
-        priority = 1000, -- load before other start plugins
-        config = function()
-            require'lush'(require'lush_theme.my_theme')
-        end
-    },
-    {
-        'romgrk/barbar.nvim',
-        dependencies = {
-            'kyazdani42/nvim-web-devicons'
-        }
-    },
-    {
-        'ggandor/leap.nvim',
-        event = "VeryLazy",
-        config = function()
-            local function leap_all_windows()
-                local focusable_windows_on_tabpage = vim.tbl_filter(
-                function (win) return vim.api.nvim_win_get_config(win).focusable end,
-                vim.api.nvim_tabpage_list_wins(0)
-                )
-                require'leap'.leap { target_windows = focusable_windows_on_tabpage }
-            end
-            vim.keymap.set('n', 's', leap_all_windows, { silent = true })
-        end
-    },
-    {
-        'folke/which-key.nvim',
-        event = "VeryLazy",
-        config = function()
-            local wk = require'which-key'
+        vim.keymap.set('n', 's', leap_all_windows, { silent = true })
+    end
+},
+{
+    'folke/which-key.nvim',
+    event = "VeryLazy",
+    config = function()
+        local wk = require'which-key'
 
-            local leader_mappings = {
-                f = {
-                    name = 'Find...',
-                    f = 'Find File',
-                    s = 'Find Symbols'
+        local leader_mappings = {
+            f = {
+                name = 'Find...',
+                f = 'Find File',
+                s = 'Find Symbols'
+            },
+            l = {
+                name = 'VimTex',
+                m = "Open Context Menu",
+                u = "Count Letters",
+                w = "Count Words",
+                d = "Open Doc for package",
+                e = "Look at the errors",
+                s = "Look at the status",
+                a = "Toggle Main",
+                v = "View pdf",
+                i = "Vimtex Info",
+                l = "Compile",
+                c = {
+                    name = "Compile",
+                    c = "Compile Project",
+                    o = "Compile Project and Show Output",
+                    s = "Compile project super fast",
+                    e = "Compile Selected",
                 },
-                l = {
-                    name = 'VimTex',
-                    m = "Open Context Menu",
-                    u = "Count Letters",
-                    w = "Count Words",
-                    d = "Open Doc for package",
-                    e = "Look at the errors",
-                    s = "Look at the status",
-                    a = "Toggle Main",
-                    v = "View pdf",
-                    i = "Vimtex Info",
-                    l = "Compile",
-                    c = {
-                        name = "Compile",
-                        c = "Compile Project",
-                        o = "Compile Project and Show Output",
-                        s = "Compile project super fast",
-                        e = "Compile Selected",
-                    },
-                    r = {
-                        name = "Reload",
-                        r = "Reload",
-                        s = "Reload State",
-                    },
-                    o = {
-                        name = "Stop",
-                        p = "Stop",
-                        a = "Stop All",
-                    },
-                    t = {
-                        name = "TOC",
-                        o = "Open TOC",
-                        t = "Toggle TOC",
-                    },
+                r = {
+                    name = "Reload",
+                    r = "Reload",
+                    s = "Reload State",
                 },
-            }
-
-            local normal_mappings = {
-                [']'] = {
-                    [']'] = 'next [sub*]section',
-                    m = 'next environment',
-                    n = 'next math zone',
-                    r = 'next frame'
-
+                o = {
+                    name = "Stop",
+                    p = "Stop",
+                    a = "Stop All",
                 },
-                ['['] = {
-                    ['['] = 'prev [sub*]section',
-                    m = 'prev environment',
-                    n = 'prev math zone',
-                    r = 'prev frame'
-
-                }
-            }
-
-            wk.register(leader_mappings, { prefix = '<leader>' })
-            wk.register(normal_mappings)
-            wk.setup{
-                ignore_missing = false
-            }
-        end
-    },
-    {
-        'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
-        config = function()
-            require'lsp_lines'.setup()
-            vim.diagnostic.config{
-                virtual_text = false,
-            }
-        end
-    },
-    {
-        'folke/noice.nvim',
-        config = true,
-        opts = {
-            lsp = {
-                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                override = {
-                    ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-                    ['vim.lsp.util.stylize_markdown'] = true,
-                    ['cmp.entry.get_documentation'] = true,
-                },
-                progress = {
-                    enabled = false,
+                t = {
+                    name = "TOC",
+                    o = "Open TOC",
+                    t = "Toggle TOC",
                 },
             },
-            -- show @recording messages
-            routes = {
-                {
-                    view = "notify",
-                    filter = { event = "msg_showmode" },
-                },
-            },
-            cmdline = {
-                format = {
-                    cmdline = { pattern = "^:", icon = ":", lang = "vim" },
-                    search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex"},
-                    search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex"},
-                    filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
-                    lua = { pattern = "^:%s*lua%s+", icon = "", lang = "lua" },
-                    help = { pattern = "^:%s*he?l?p?%s+", icon = "", lang = "bash" },
-                    search_replace = { icon = "", kind = "search", title = " Search and Replace ", pattern = "^:%s*%%s/", lang = "regex", conceal = false },
-                },
-            },
-            messages = {
-                view_search = false
-            }
-
-        },
-        dependencies = {
-            'MunifTanjim/nui.nvim',
-            'rcarriga/nvim-notify'
-        },
-    },
-    {
-        'rcarriga/nvim-notify', -- also used for noice.nvim
-        config = function()
-            require'notify'.setup{
-                fps = 60,
-                icons = {
-                    DEBUG = " ",
-                    ERROR = " ",
-                    INFO = " ",
-                    TRACE = "✎ ",
-                    WARN = " "
-                },
-                level = 2,
-                minimum_width = 50,
-                render = "default",
-                stages = "fade",
-                timeout = 5000,
-                top_down = true
-            }
-            vim.notify = require'notify'
-        end
-    },
-    {
-        'martineausimon/nvim-lilypond-suite',
-        ft = "ly",
-        dependencies = {
-            'MunifTanjim/nui.nvim'
         }
+
+        local normal_mappings = {
+            [']'] = {
+                [']'] = 'next [sub*]section',
+                m = 'next environment',
+                n = 'next math zone',
+                r = 'next frame'
+
+            },
+            ['['] = {
+                ['['] = 'prev [sub*]section',
+                m = 'prev environment',
+                n = 'prev math zone',
+                r = 'prev frame'
+
+            }
+        }
+
+        wk.register(leader_mappings, { prefix = '<leader>' })
+        wk.register(normal_mappings)
+        wk.setup{
+            ignore_missing = false
+        }
+    end
+},
+{
+    'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
+    config = function()
+        require'lsp_lines'.setup()
+        vim.diagnostic.config{
+            virtual_text = false,
+        }
+    end
+},
+{
+    'folke/noice.nvim',
+    config = true,
+    opts = {
+        lsp = {
+            -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+            override = {
+                ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+                ['vim.lsp.util.stylize_markdown'] = true,
+                ['cmp.entry.get_documentation'] = true,
+            },
+            progress = {
+                enabled = false,
+            },
+        },
+        -- show @recording messages
+        routes = {
+            {
+                view = "notify",
+                filter = { event = "msg_showmode" },
+            },
+        },
+        cmdline = {
+            format = {
+                cmdline = { pattern = "^:", icon = ":", lang = "vim" },
+                search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex"},
+                search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex"},
+                filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+                lua = { pattern = "^:%s*lua%s+", icon = "", lang = "lua" },
+                help = { pattern = "^:%s*he?l?p?%s+", icon = "", lang = "bash" },
+                search_replace = { icon = "", kind = "search", title = " Search and Replace ", pattern = "^:%s*%%s/", lang = "regex", conceal = false },
+            },
+        },
+        messages = {
+            view_search = false
+        }
+
     },
+    dependencies = {
+        'MunifTanjim/nui.nvim',
+        'rcarriga/nvim-notify'
+    },
+},
+{
+    'rcarriga/nvim-notify', -- also used for noice.nvim
+    config = function()
+        require'notify'.setup{
+            fps = 60,
+            icons = {
+                DEBUG = " ",
+                ERROR = " ",
+                INFO = " ",
+                TRACE = "✎ ",
+                WARN = " "
+            },
+            level = 2,
+            minimum_width = 50,
+            render = "default",
+            stages = "fade",
+            timeout = 5000,
+            top_down = true
+        }
+        vim.notify = require'notify'
+    end
+},
+{
+    'martineausimon/nvim-lilypond-suite',
+    ft = "ly",
+    dependencies = {
+        'MunifTanjim/nui.nvim'
+    }
+},
 }
