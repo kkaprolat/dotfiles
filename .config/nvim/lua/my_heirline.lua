7b92c40c259c7e9928a0a04535fde860a10588c3
Kay Kaprolat <kakaoh6@gmail.com>
2023-02-16 08:09:48 +0100

    nvim: fix search component of heirline

--- .config/nvim/lua/my_heirline.lua
+++ .config/nvim/lua/my_heirline.lua
@@ -375,36 +375,19 @@ local Updates = {
 
 
 local SearchResults = {
-    condition = function(self)
-        local lines = vim.api.nvim_buf_line_count(0)
-        if lines > 50000 then return end
-
-        local query = vim.fn.getreg("/")
-        if query == "" then return end
-
-        if query:find("@") then return end
-
-        local search_count = vim.fn.searchcount({ recompute = 1, maxcount = -1})
-        local active = false
-        if vim.v.hlsearch and vim.v.hlsearch == 1 and search_count.total > 0 then
-            active = true
-        end
-        if not active then return end
-
-        query = query:gsub([[^\V]], "")
-        query = query:gsub([[\<]], ""):gsub([[\>]], "")
-
-        self.query = query
-        self.count = search_count
-        return true
-    end,
     {
+        condition = function()
+            return vim.v.hlsearch ~= 0 and vim.fn.getreg("/") ~= ""
+        end,
         RightCapStatic(colors.green),
         {
-            provider = function(self)
-                return table.concat({
-                    ' ', self.query, ' ', self.count.current, '/', self.count.total, ' '
-                })
+            provider = function()
+                local ok, search = pcall(vim.fn.searchcount)
+                if ok and search.total then
+                    return ' ' .. vim.fn.getreg("/") .. ' ' .. string.format("%d/%d", search.current, math.min(search.total, search.maxcount))
+                else
+                    return "ERROR"
+                end
             end,
             hl = colors.green
         },
