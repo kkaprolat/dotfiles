2eb4e67d4479285b4aa35d450de1a16cee38dbaf
Kay Kaprolat <Frank_Kay.Kaprolat@mailbox.tu-dresden.de>
2023-07-10 17:08:36 +0200

    nvim: change diagnostics signs

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -203,7 +203,7 @@ vim.lsp.handlers["$/progress"] = function(_, result, ctx)
     end
 end
 
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
