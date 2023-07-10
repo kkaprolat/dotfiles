f0ade4e3d1129fd99cb3e7a5ac104000f3116798
Kay Kaprolat <Frank_Kay.Kaprolat@mailbox.tu-dresden.de>
2023-07-10 17:11:11 +0200

    nvim: remove lsp_lines

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -322,15 +322,6 @@ return {
     end
 },
 {
-    'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
-    config = function()
-        require'lsp_lines'.setup()
-        vim.diagnostic.config{
-            virtual_text = false,
-        }
-    end
-},
-{
     'folke/noice.nvim',
     config = true,
     opts = {
