08f4f4bc3f60645717d13711701d1f44982317a8
Kay Kaprolat <Frank_Kay.Kaprolat@mailbox.tu-dresden.de>
2023-07-10 10:51:29 +0200

    nvim: remember last place in file

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -72,3 +72,22 @@ o.cmdheight = 0
 
 -- don't notify when mode changes
 o.showmode = false
+
+-- see https://www.reddit.com/r/neovim/comments/1052d98/comment/j39bgco/?utm_source=share&utm_medium=web2x&context=3
+-- and https://github.com/creativenull/dotfiles/blob/9ae60de4f926436d5682406a5b801a3768bbc765/config/nvim/init.lua#L70-L86
+local lastplace = vim.api.nvim_create_augroup("LastPlace", {})
+vim.api.nvim_clear_autocmds({ group = lastplace })
+vim.api.nvim_create_autocmd("BufReadPost", {
+    group = lastplace,
+    pattern = { "*" },
+    desc = "remember last cursor place",
+    callback = function(args)
+        local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line('$')
+        local not_commit = vim.b[args.buf].filetype ~= 'commit'
+
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        if valid_line and not_commit then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+    end,
+})
