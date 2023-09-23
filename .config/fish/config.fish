3625cc03ba650e32ecc259cc361536cd2d8e43f2
Kay Kaprolat <Frank_Kay.Kaprolat@mailbox.tu-dresden.de>
2023-09-23 17:32:58 +0200

    fish: use starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set -x STARSHIP_CONFIG ~/.config/starship/starship.toml
+    starship init fish | source
 end
