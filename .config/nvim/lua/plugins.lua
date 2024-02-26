12ee7a16afd1b25431588bbfba9d5addf91e4d88
Kay Kaprolat <kakaoh6@gmail.com>
2024-02-26 08:14:32 +0100

    nvim: set default trouble to workspace

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -535,10 +535,11 @@ return {
             auto_open = true,
             auto_close = false,
             use_diagnostic_signs = true,
-            mode = 'document_diagnostics',
+            mode = 'workspace_diagnostics',
             padding = false
         }
         vim.keymap.set("n", "<leader>td", "<cmd>TroubleToggle document_diagnostics<CR>")
+        vim.keymap.set("n", "<leader>tw", "<cmd>TroubleToggle workspace_diagnostics<CR>")
         vim.keymap.set("n", "<leader>tr", "<cmd>TroubleRefresh<CR>")
     end,
 },
