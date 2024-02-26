9f55419d6cc6cee0b8c8c426b1a117cf64556f66
Kay Kaprolat <kakaoh6@gmail.com>
2024-02-26 08:14:05 +0100

    nvim: only leap in single window

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -245,14 +245,10 @@ return {
     'ggandor/leap.nvim',
     event = "VeryLazy",
     config = function()
-        local function leap_all_windows()
-            local focusable_windows_on_tabpage = vim.tbl_filter(
-            function (win) return vim.api.nvim_win_get_config(win).focusable end,
-            vim.api.nvim_tabpage_list_wins(0)
-            )
-            require'leap'.leap { target_windows = focusable_windows_on_tabpage }
+        local function leap_current_window()
+            require'leap'.leap { target_windows = { vim.fn.win_getid() } }
         end
-        vim.keymap.set('n', 's', leap_all_windows, { silent = true })
+        vim.keymap.set('n', 's', leap_current_window, { silent = true })
     end
 },
 {
